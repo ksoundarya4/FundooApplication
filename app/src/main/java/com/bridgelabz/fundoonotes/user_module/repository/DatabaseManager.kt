edfb4e6ca5ab0b4bec20ab06929ca71b4c264231
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-30 18:57:30 +0530

    [soundarya]Add. Added DatabaseManager.kt interface

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/user_module/repository/DatabaseManager.kt
@@ -0,0 +1,22 @@
+/**
+ * Fundoo Notes
+ * @description DatabaseManager Interface to perform
+ * actions on user database.
+ * @file DatabaseManager.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 30/01/2020
+ */
+package com.bridgelabz.fundoonotes.user_module.repository
+
+import android.database.Cursor
+import com.bridgelabz.fundoonotes.user_module.registration.model.User
+
+interface DatabaseManager {
+    fun open(): DatabaseManager
+    fun close()
+    fun insert(user : User,description : String)
+    fun fetch(): Cursor
+    fun update(KEY_ID : Long, user : User , description : String): Int
+    fun delete()
+}
