ab4aebf08a5944b4c5bc7e271c15342057a799ec
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-30 11:31:25 +0530

    [soundarya]Refactor. Added file and function comments to RegexUtil.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/user_module/regex_util/RegexUtil.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/user_module/regex_util/RegexUtil.kt
@@ -1,19 +1,31 @@
+/**
+ * Fundoo Notes
+ * @description RegexUtil Class to validate User
+ * properties.
+ * @file RegexUtil.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 30/01/2020
+ */
 package com.bridgelabz.fundoonotes.user_module.regex_util
 
 import java.util.regex.Pattern
 
 class RegexUtil {
 
+    /**Function to validate User first name*/
     fun validateFirstName(firstName: String) {
 
     }
 
+    /** Function to validate user email address*/
     fun validateEmail(email: String): Boolean {
         val emailExpression = "^[A-Za-z0-9+_.-]+@(.+)$"
         val emailPattern = Pattern.compile(emailExpression)
         return emailPattern.matcher(email).matches()
     }
 
+    /** Function to validate user email password*/
     fun validatePassword(password: String): Boolean {
         val passwordExpression = "(?=.*[a-zA-Z])" + ".{5,})" //at least 5 character
         val passwordPattern = Pattern.compile(passwordExpression)
