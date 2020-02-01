5751f2631d236e3959e2ad94e38cc66a37ddb929
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-01 17:57:04 +0530

    [soundarya]Refactor. RegisterActivity.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/user_module/registration/view/RegisterActivity.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/user_module/registration/view/RegisterActivity.kt
@@ -1,7 +1,6 @@
 package com.bridgelabz.fundoonotes.user_module.registration.view
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.bridgelabz.fundoonotes.R
@@ -13,45 +12,54 @@ import com.google.android.material.textfield.TextInputEditText
 class RegisterActivity : AppCompatActivity() {
 
     lateinit var registerViewModel: RegisterViewModel
-
-    private val firstName by lazy {
-        findViewById<TextInputEditText>(R.id.first_name)
-    }
-    private val lastName by lazy {
-        findViewById<TextInputEditText>(R.id.last_name_)
-    }
-    private val dateOfBirth by lazy {
-        findViewById<TextInputEditText>(R.id.date_of_birth)
-    }
-    private val email by lazy {
-        findViewById<TextInputEditText>(R.id.user_email)
-    }
-    private val password by lazy {
-        findViewById<TextInputEditText>(R.id.password)
-    }
-    private val phoneNumber by lazy {
-        findViewById<TextInputEditText>(R.id.phoneNumber)
-    }
-    lateinit var user: User
+    lateinit var firstName: TextInputEditText
+    lateinit var lastName: TextInputEditText
+    lateinit var dateOfBirth: TextInputEditText
+    lateinit var email: TextInputEditText
+    lateinit var password: TextInputEditText
+    lateinit var phoneNumber: TextInputEditText
+    lateinit var signUPButton :Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        onViews()
         registerViewModel = RegisterViewModel(UserDbHelper(this))
 
-        user = User(
-            firstName.editableText.toString(),
-            lastName.editableText.toString(),
-            dateOfBirth.editableText.toString(),
-            email.editableText.toString(),
-            password.editableText.toString(),
+
+
+        onClickListener()
+
+
+
+
+
+
+    }
+
+    private fun onViews() {
+        firstName = findViewById(R.id.first_name)
+        lastName = findViewById(R.id.last_name_)
+        dateOfBirth = findViewById(R.id.date_of_birth)
+        email = findViewById(R.id.user_email)
+        password = findViewById(R.id.password)
+        phoneNumber = findViewById(R.id.phoneNumber)
+        signUPButton =findViewById<Button>(R.id.button_sign_up)
+    }
+
+    private fun onClickListener(){
+        signUPButton.setOnClickListener {
+
+            val fname =firstName.editableText.toString()
+            val lname =lastName.editableText.toString()
+            val dob =dateOfBirth.editableText.toString()
+            email.editableText.toString()
+            password.editableText.toString()
             phoneNumber.editableText.toString()
-        )
+            registerViewModel.validateUser(User(
+                "fj", "foas", "22/10/9182", "foiej@gmail","773ffee2", "423423423523"))
 
-        Log.d("UserInfo","$user")
-
-        val signUPButton = findViewById<Button>(R.id.button_sign_up)
-        signUPButton.setOnClickListener { registerViewModel.validateUser(user) }
+        }
     }
 }
