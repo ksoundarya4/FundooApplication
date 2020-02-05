707c0a16cf4d51c2788d1b8a2071ca13c0549748
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-05 18:37:33 +0530

    [soundarya]Add. Added note_module to Fundoo Notes Application

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/DashBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+    }
+}
---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.view.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
