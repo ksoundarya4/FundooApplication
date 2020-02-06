a0964acb83a1dd70ce44ca160a8cda7091b2e8bb
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-06 18:05:43 +0530

    [soundarya]Add. adding navigation view to DashBoardActivity.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/DashBoardActivity.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/DashBoardActivity.kt
@@ -1,13 +1,19 @@
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bridgelabz.fundoonotes.R
+import com.google.android.material.navigation.NavigationView
 
-class DashBoardActivity : AppCompatActivity() {
+class DashBoardActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dash_board)
+        setContentView(R.layout.app_bar_menu)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 }
