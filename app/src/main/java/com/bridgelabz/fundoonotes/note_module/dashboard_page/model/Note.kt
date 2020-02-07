bb032d5225c5e03b00aabf610e3ea950e74d66b7
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-07 11:43:27 +0530

    [soundarya]Add. Added Note.kt into note_module

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/model/Note.kt
@@ -0,0 +1,12 @@
+/**
+ * Fundoo Notes
+ * @description Note data class which contains
+ * Note title and description.
+ * @file Note.kt
+ * @author ksoundarya4
+ * @version 1.0
+ *@since 07/02/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.model
+
+data class Note(val tile: String, val description: String)
