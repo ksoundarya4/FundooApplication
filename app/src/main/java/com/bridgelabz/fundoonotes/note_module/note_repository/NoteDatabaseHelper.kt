48ff7f669051506aa3bce4c6bc475f7c5ff6a7cc
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-07 12:51:35 +0530

    [soundarya]Add. Added NoteDatabaseHelper.kt into note_repository

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_repository/NoteDatabaseHelper.kt
@@ -0,0 +1,54 @@
+/**
+ * Fundoo Notes
+ * @description NoteDatabaseHelper class extends SQLiteOpenHelper
+ * contains context and used to create FundooNotes table inside
+ * FundooNotes.db database.
+ * @file ksoundarya4
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 07/02/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.note_repository
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+/**Constant that create table inside database*/
+const val CREATE_TABLE = "Create table ${NoteRegistrationContract.NoteEntry.TABLE_NAME} " +
+        "(" +
+        " ${BaseColumns._ID} INTEGER PRIMARY KEY ," +
+        "${NoteRegistrationContract.NoteEntry.KEY_TITLE} TEXT , " +
+        "${NoteRegistrationContract.NoteEntry.KEY_DESCRIPTION} TEXT" +
+        ")"
+
+/**Constant that drops the table*/
+const val DROP_ENTRIES = "DROP TABLE IF EXISTS ${NoteRegistrationContract.NoteEntry.TABLE_NAME}"
+
+class NoteDatabaseHelper(context: Context) : SQLiteOpenHelper(
+    context, DATABASE_NAME, null,
+    DATABASE_VERSION
+) {
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "FundooNotes.db"
+    }
+
+    /**Function to create FundooNotes.db database*/
+    override fun onCreate(database: SQLiteDatabase) {
+        database.execSQL(CREATE_TABLE)
+    }
+
+    /**Function to up grade existing database*/
+    override fun onUpgrade(database: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        database.execSQL(DROP_ENTRIES)
+        onCreate(database)
+    }
+
+    /**Function to downgrade existing database*/
+    override fun onDowngrade(database: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        database.execSQL(DROP_ENTRIES)
+        onCreate(database)
+    }
+}
