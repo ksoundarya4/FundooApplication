8adedbb8ad3b3777d781469eb7d36f7507df5bd3
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-07 12:24:42 +0530

    [soundarya]Add. Added NoteRegistrationContract.kt to note_repository

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_repository/NoteRegistrationContract.kt
@@ -0,0 +1,21 @@
+/**
+ * Fundoo Notes
+ * @description NoteRegistrationContract contain
+ * NoteEntry which extends BaseColumns and has
+ * TABLE_NAME,KEY_TITLE and KEY_DESCRIPTION.
+ * @file NoteRegistrationContract.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 07/02/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.note_repository
+
+import android.provider.BaseColumns
+
+object NoteRegistrationContract {
+    object NoteEntry : BaseColumns {
+        const val TABLE_NAME = "FundooNotes"
+        const val KEY_TITLE = "Title"
+        const val KEY_DESCRIPTION = "Description"
+    }
+}
