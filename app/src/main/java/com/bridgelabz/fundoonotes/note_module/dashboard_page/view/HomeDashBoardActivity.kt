c5c8f9ca251af76a76ddf84d0292695583db9405
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-10 19:08:39 +0530

    [soundarya]Refactor. Removed appBarConfiguration from HomeDashBoardActivity.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/HomeDashBoardActivity.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/HomeDashBoardActivity.kt
@@ -1,23 +1,29 @@
+/**
+ * Fundoo Notes
+ * @description HomeDashBoardActivity displays all user actions.
+ * @file HomeDashBoardActivity.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 07/02/2020
+ */
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.bridgelabz.fundoonotes.R
 import com.bridgelabz.fundoonotes.note_module.note_page.view.AddNoteFragment
+import com.bridgelabz.fundoonotes.user_module.login.view.LoginActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 class HomeDashBoardActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +32,42 @@ class HomeDashBoardActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    drawerLayout.closeDrawer(navigationView)
+                    navigateToHome()
+                    Toast.makeText(this, "Home Tapped", Toast.LENGTH_SHORT).show()
+                    return@setNavigationItemSelectedListener true
+                }
+                R.id.nav_sing_out -> {
+                    navigateToLoginScreen()
+                    Toast.makeText(this, "Signing Out", Toast.LENGTH_LONG).show()
+                    return@setNavigationItemSelectedListener true
+                }
+                else -> return@setNavigationItemSelectedListener false
+            }
+        }
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        actionBarDrawerToggle.syncState()
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener {
-           navigateToAddNoteFragment()
+            navigateToAddNoteFragment()
         }
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_sing_out
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -52,15 +76,35 @@ class HomeDashBoardActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_sign_out -> {
+                navigateToLoginScreen()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 
     private fun navigateToAddNoteFragment() {
         val fragment = AddNoteFragment()
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.drawer_layout,fragment)
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    private fun navigateToHome() {
+        val fragment = HomeFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun navigateToLoginScreen() {
+        Intent(this, LoginActivity::class.java).apply {
+            startActivity(this)
+        }
     }
 }
