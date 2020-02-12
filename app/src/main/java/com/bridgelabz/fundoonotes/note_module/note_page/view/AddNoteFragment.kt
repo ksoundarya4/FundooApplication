a13edd2e9e368709373cb1c8c481dce58e8be6b6
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-12 12:48:10 +0530

    [soundarya]Refactor. Added conditions to insert only not empty notes in AddNoteFragment.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/AddNoteFragment.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/AddNoteFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -32,8 +33,12 @@ class AddNoteFragment : Fragment() {
         saveNoteButton.setOnClickListener {
             val noteTitle = title.editableText.toString()
             val noteDescription = description.editableText.toString()
-            note = Note(noteTitle, noteDescription)
-            setClickListener(view, note)
+            if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
+                note = Note(noteTitle, noteDescription)
+                setClickListener(view, note)
+            } else {
+                Toast.makeText(view.context, "Empty note discarded", Toast.LENGTH_LONG).show()
+            }
         }
         return view
     }
