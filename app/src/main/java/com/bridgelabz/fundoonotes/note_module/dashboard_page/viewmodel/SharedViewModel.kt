8c3d1893189778ecfb1df02701fdcef800a9ce05
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-13 19:11:42 +0530

    [soundarya]Refactor. moved noteDbManager to contructor of ShareViewModel.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
@@ -1,24 +1,16 @@
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel
 
-import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
-import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper
 import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteDatabaseManager
-import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteDatabaseManagerImpl
 
-class SharedViewModel : ViewModel() {
+class SharedViewModel(private val noteDbManager : NoteDatabaseManager) : ViewModel() {
 
-    private lateinit var noteDbManager: NoteDatabaseManager
     private val noteLiveData = MutableLiveData<ArrayList<Note>>()
 
-    fun onSaveButtonClick(view: View, note: Note) {
-        noteDbManager =
-            NoteDatabaseManagerImpl(
-                DatabaseHelper(view.context)
-            )
+    fun onSaveButtonClick(note: Note) {
         val rowId = noteDbManager.insert(note)
         if (rowId > 0)
             fetchNotes()
