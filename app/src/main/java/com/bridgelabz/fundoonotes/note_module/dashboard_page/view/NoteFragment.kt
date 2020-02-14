b0b8155e8e85b8afb195d091c4e82a65f4df4e6c
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-14 19:03:54 +0530

    [soundarya]Refactor. Added RecyclerView implementations in NoteFragment.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteFragment.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteFragment.kt
@@ -5,24 +5,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bridgelabz.fundoonotes.R
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel.NoteDbManagerFactory
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel.SharedViewModel
+import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper
+import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteDatabaseManagerImpl
 
 class NoteFragment : Fragment() {
 
-    private lateinit var sharedViewModel: SharedViewModel
+    private val noteFactory by lazy {
+        NoteDbManagerFactory(NoteDatabaseManagerImpl(DatabaseHelper(requireContext())))
+    }
+    private val sharedViewModel: SharedViewModel by lazy {
+        ViewModelProvider(this, noteFactory).get(SharedViewModel::class.java)
+    }
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var noteAdapter: NoteAdapter
+    private lateinit var noteViewManager: RecyclerView.LayoutManager
+    private lateinit var notes: ArrayList<Note>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        sharedViewModel =
-//            ViewModelProviders.of(this).get(SharedViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_home)
-//        sharedViewModel.getNoteLiveData()
-//            .observe(this, Observer { textView.text = "home Drawer"})
-        return root
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        findViews(view)
+        return view
+    }
+
+    private fun findViews(view: View) {
+        recyclerView = view.findViewById(R.id.notes_recycler_view)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        sharedViewModel.getNoteLiveData()
+            .observe(requireActivity(), Observer { getNotes(it) })
+        noteAdapter = NoteAdapter(notes)
+        noteViewManager = LinearLayoutManager(requireContext())
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = noteViewManager
+            adapter = noteAdapter
+        }
+    }
+
+    private fun getNotes(notes: List<Note>?) {
+        this.notes = notes as ArrayList<Note>
     }
 }
