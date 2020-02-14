775c0e4668633557ba61d02990907dd32e878a86
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-14 15:05:11 +0530

    [soundarya]Refactor. Renamed as NoteFragment.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteFragment.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.bridgelabz.fundoonotes.R
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel.SharedViewModel
 
-class HomeFragment : Fragment() {
+class NoteFragment : Fragment() {
 
     private lateinit var sharedViewModel: SharedViewModel
 
@@ -20,12 +17,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        sharedViewModel =
-            ViewModelProviders.of(this).get(SharedViewModel::class.java)
+//        sharedViewModel =
+//            ViewModelProviders.of(this).get(SharedViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        sharedViewModel.getNoteLiveData()
-            .observe(this, Observer { textView.text = "home Drawer"})
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        sharedViewModel.getNoteLiveData()
+//            .observe(this, Observer { textView.text = "home Drawer"})
         return root
     }
 }
