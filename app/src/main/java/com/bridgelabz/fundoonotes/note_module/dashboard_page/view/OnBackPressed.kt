e3990532324d5e8e25c301d70bc4d1212585176e
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-14 12:54:10 +0530

    [soundarya]Add. Added OnBackPressed.kt interface

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/OnBackPressed.kt
@@ -0,0 +1,15 @@
+/**
+ * Fundoo Notes
+ * @description OnBackPressedInFragment interface
+ * will have onBackPressed function which performs
+ * action when back button is pressed in fragment.
+ * @file OnBackPressedFragment
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/02/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
+
+interface OnBackPressed {
+    fun onBackPressed()
+}
