e72859c00fc43d02c1c555b67a88ee6f5993dad3
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-15 13:07:22 +0530

    [soundarya]Refactor. Removed if condition from onSaveButtonClick and added fetchNotes() into getNoteLiveData function in SharedViewModel.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
@@ -6,21 +6,20 @@ import androidx.lifecycle.ViewModel
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
 import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteDatabaseManager
 
-class SharedViewModel(private val noteDbManager : NoteDatabaseManager) : ViewModel() {
+class SharedViewModel(private val noteDbManager: NoteDatabaseManager) : ViewModel() {
 
-    private val noteLiveData = MutableLiveData<List<Note>>()
+    private val noteLiveData = MutableLiveData<ArrayList<Note>>()
 
     fun onSaveButtonClick(note: Note) {
-        val rowId = noteDbManager.insert(note)
-        if (rowId > 0)
-            fetchNotes()
+        noteDbManager.insert(note)
     }
 
     private fun fetchNotes() {
         noteLiveData.value = noteDbManager.fetch()
     }
 
-    fun getNoteLiveData(): LiveData<List<Note>> {
+    fun getNoteLiveData(): LiveData<ArrayList<Note>> {
+        fetchNotes()
         return noteLiveData
     }
 }
