7e8c1be2d8f352ab106f22a968784304d3968de7
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-17 11:05:44 +0530

    [soundarya]Refactor. Added file comment and changed data type of notes to ArrayList in NoteViewAdapter.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteViewAdapter.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteViewAdapter.kt
@@ -1,46 +1,25 @@
+/**
+ * Fundoo Notes
+ * @description NoteViewAdapter class extends RecyclerView.Adapter
+ * which holds functions that deals with implementation of RecyclerView.
+ * @file NoteViewAdapter.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 15/02/2020
+ */
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
 
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.view.ActionMode
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bridgelabz.fundoonotes.R
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
 
-
-class NoteViewAdapter(private var notes: List<Note>) :
+class NoteViewAdapter(private var notes: ArrayList<Note>) :
     RecyclerView.Adapter<NoteViewAdapter.NoteViewHolder>() {
-
-    private var multiSelect = false
-    private val selectedItems = ArrayList<Int>()
-
-    val actionModeCallBacks: ActionMode.Callback =
-        object : ActionMode.Callback {
-            override fun onCreateActionMode(
-                mode: ActionMode?,
-                menu: Menu?
-            ): Boolean {
-                multiSelect = true
-                menu?.add("Delete")
-                return true
-            }
-
-            override fun onPrepareActionMode(
-                mode: ActionMode?,
-                menu: Menu?
-            ): Boolean {
-                return false
-            }
-
-            override fun onActionItemClicked(
-                mode: ActionMode?,
-                item: MenuItem?
-            ): Boolean {
-                return false
-            }
-
-            override fun onDestroyActionMode(mode: ActionMode) {}
-        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val itemView =
@@ -58,11 +37,12 @@ class NoteViewAdapter(private var notes: List<Note>) :
         holder.description.text = note.description
     }
 
-    fun setListOfNotes(notes: List<Note>) {
+    fun setListOfNotes(notes: ArrayList<Note>) {
         this.notes = notes
     }
 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardView: CardView = itemView.findViewById(R.id.note_card_view)
         val title: TextView = itemView.findViewById(R.id.text_view_note_title)
         val description: TextView = itemView.findViewById(R.id.text_view_note_description)
     }
