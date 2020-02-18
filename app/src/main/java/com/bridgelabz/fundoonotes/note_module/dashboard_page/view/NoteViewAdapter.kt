ca8d87a3c6cc1193934a2deebf316c9815ee3de7
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-18 17:21:59 +0530

    [soundarya]Refactor. Added RecyclerClickListener reference in NoteViewAdapter.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteViewAdapter.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteViewAdapter.kt
@@ -15,8 +15,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bridgelabz.fundoonotes.R
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
 
-class NoteViewAdapter(private var notes: ArrayList<Note>) :
+class NoteViewAdapter(
+    private var notes: ArrayList<Note>,
+    private val onItemClickListener: RecyclerClickListener
+) :
     RecyclerView.Adapter<NoteViewHolder>() {
+
+//    private var selectedItemsIds = SparseBooleanArray()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val itemView =
@@ -30,10 +35,42 @@ class NoteViewAdapter(private var notes: ArrayList<Note>) :
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val note = notes[position]
-        holder.bindNote(note)
+        holder.bindNote(note, onItemClickListener)
     }
 
+    /**Set array of notes*/
     fun setListOfNotes(notes: ArrayList<Note>) {
         this.notes = notes
     }
+//
+//    /**Toggle selection function*/
+//    fun toggleSelection(position: Int) {
+//        selectView(position = position, value = !selectedItemsIds.get(position))
+//    }
+//
+//    /**Remove selected selection */
+//    fun removeSelection() {
+//        selectedItemsIds = SparseBooleanArray()
+//        notifyDataSetChanged()
+//    }
+//
+//    /**Put or delete selected item view position
+//     * into SparseBooleanArray*/
+//    private fun selectView(position: Int, value: Boolean) {
+//        if (value)
+//            selectedItemsIds.put(position, value)
+//        else
+//            selectedItemsIds.delete(position)
+//        notifyDataSetChanged()
+//    }
+//
+//    /**Get total selected item view count*/
+//    fun getSelectedItemCount(): Int {
+//        return selectedItemsIds.size()
+//    }
+//
+//    /**Return all selected item view ids*/
+//    fun getSelectedItem(): SparseBooleanArray {
+//        return selectedItemsIds
+//    }
 }
