3315dbd55875445e4b660c6c436759cfcc087cde
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-19 18:49:42 +0530

    [soundarya]Refactor. Added handleNote,getLiveNote and updateNoteOnClick functions to SharedViewModel.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
@@ -1,5 +1,6 @@
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,18 +9,34 @@ import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteDatab
 
 class SharedViewModel(private val noteDbManager: NoteDatabaseManager) : ViewModel() {
 
-    private val noteLiveData = MutableLiveData<ArrayList<Note>>()
+    private val notesLiveData = MutableLiveData<ArrayList<Note>>()
+    private val noteLiveData = MutableLiveData<Note>()
+    private var rowId: Long? = null
 
     fun onSaveButtonClick(note: Note) {
         noteDbManager.insert(note)
     }
 
     private fun fetchNotes() {
-        noteLiveData.value = noteDbManager.fetch()
+        notesLiveData.value = noteDbManager.fetchNotes()
     }
 
     fun getNoteLiveData(): LiveData<ArrayList<Note>> {
         fetchNotes()
+        return notesLiveData
+    }
+
+    fun handleNoteAt(note: Note) {
+        noteLiveData.value = note
+        rowId = noteDbManager.fetchNoteId(note)
+        Log.d("note", note.toString())
+    }
+
+    fun getLiveNote(): LiveData<Note> {
         return noteLiveData
+    }
+
+    fun updateNoteOnClick(note: Note) {
+        noteDbManager.updateNote(rowId!!, note)
     }
 }
