d74c38d542f619cc023a36d3d856e90f003fe578
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-21 19:01:33 +0530

    [soundarya]Refactor. removed UserRegistartionContract and NoteRegistrationContract from DatabaseHelper.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/local_service/DatabaseHelper.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/local_service/DatabaseHelper.kt
@@ -4,25 +4,9 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
-import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper.NoteRegistrationContract.NoteEntry.TABLE_NOTE
 import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper.UserNoteRegistrationContract.UserNote.TABLE_USER_NOTE
-import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper.UserRegistrationContract.UserEntry.TABLE_USER
-
-private const val CREATE_USER_TABLE =
-    " Create Table $TABLE_USER (" +
-            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
-            "${DatabaseHelper.UserRegistrationContract.UserEntry.KEY_FIRSTNAME} CHAR(15)," +
-            "${DatabaseHelper.UserRegistrationContract.UserEntry.KEY_LASTNAME} CHAR(15)," +
-            "${DatabaseHelper.UserRegistrationContract.UserEntry.KEY_DOB} CHAR(10)," +
-            "${DatabaseHelper.UserRegistrationContract.UserEntry.KEY_EMAIL} VARCHAR(200)," +
-            "${DatabaseHelper.UserRegistrationContract.UserEntry.KEY_PASSWORD} VARCHAR(20)," +
-            "${DatabaseHelper.UserRegistrationContract.UserEntry.KEY_PHONE_NUMBER} CHAR(20))"
-
-private const val CREATE_NOTE_TABLE =
-    " Create Table $TABLE_NOTE ( " +
-            "${BaseColumns._ID} INTEGER PRIMARY KEY, " +
-            "${DatabaseHelper.NoteRegistrationContract.NoteEntry.KEY_TITLE} TEXT NOT NULL, " +
-            "${DatabaseHelper.NoteRegistrationContract.NoteEntry.KEY_DESCRIPTION} TEXT NOT NULL)"
+import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteDatabaseManagerImpl.NoteEntry.CREATE_NOTE_TABLE
+import com.bridgelabz.fundoonotes.repository.local_service.user_module.UserDbManagerImpl.UserEntry.CREATE_USER_TABLE
 
 private const val CREATE_USER_NOTE_TABLE =
     "Create Table If Not Exists $TABLE_USER_NOTE (" +
@@ -68,26 +52,6 @@ class DatabaseHelper(context: Context) :
         const val VERSION_THREE = 3
         const val DATABASE_VERSION = VERSION_ONE
         const val DATABASE_NAME = "App.db"
-    }
-
-    object UserRegistrationContract {
-        object UserEntry : BaseColumns {
-            const val TABLE_USER = "UserEntry"
-            const val KEY_FIRSTNAME = "FirstName"
-            const val KEY_LASTNAME = "LastName"
-            const val KEY_DOB = "DateOfBirth"
-            const val KEY_EMAIL = "Email"
-            const val KEY_PASSWORD = "Password"
-            const val KEY_PHONE_NUMBER = "PhoneNumber"
-        }
-    }
-
-    object NoteRegistrationContract {
-        object NoteEntry : BaseColumns {
-            const val TABLE_NOTE = "Notes"
-            const val KEY_TITLE = "Title"
-            const val KEY_DESCRIPTION = "Description"
-        }
     }
 
     object UserNoteRegistrationContract {
