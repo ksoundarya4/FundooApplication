830668a1f95dbff792046ce97125ac42971d7589
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-22 12:55:19 +0530

    [soundarya]Add. Added RecyclerViewType.kt enum class

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/model/RecyclerViewType.kt
@@ -0,0 +1,14 @@
+/**
+ * Fundoo Notes
+ * @description RecyclerViewTyper enum holds GridView
+ * and ListView types.
+ * @file RecyclerViewType.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 22/01/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.model
+
+enum class RecyclerViewType {
+    GridView, ListView
+}
