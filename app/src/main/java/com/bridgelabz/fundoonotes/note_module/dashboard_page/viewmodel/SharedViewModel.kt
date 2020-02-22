2a5407a667c5426714addba09c68e4313d246b56
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-22 15:52:15 +0530

    [soundarya]Refactor. Added getRecyclerViewType and setRecyclerViewType functions in SharedViewModel.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/SharedViewModel.kt
@@ -1,3 +1,11 @@
+/**
+ * Fundoo Notes
+ * @description SharedViewModel extends ViewModel
+ * @file SharedViewModel.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 12/02/2020
+ */
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel
 
 import android.util.Log
@@ -5,11 +13,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.view.recycler_view_strategy.RecyclerViewType
 import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteDatabaseManager
 
 class SharedViewModel(private val noteDbManager: NoteDatabaseManager) : ViewModel() {
 
     private val notesLiveData = MutableLiveData<ArrayList<Note>>()
+    private val recyclerViewTypeLiveData = MutableLiveData<RecyclerViewType>()
     private var rowId: Long? = null
 
     fun insertNoteOnCLick(note: Note) {
@@ -32,5 +42,13 @@ class SharedViewModel(private val noteDbManager: NoteDatabaseManager) : ViewMode
 
     fun updateNoteOnClick(note: Note) {
         noteDbManager.updateNote(rowId!!, note)
+    }
+
+    fun getRecyclerViewType(): LiveData<RecyclerViewType> {
+        return recyclerViewTypeLiveData
+    }
+
+    fun setRecyclerViewType(recyclerViewType: RecyclerViewType) {
+        recyclerViewTypeLiveData.value = recyclerViewType
     }
 }
