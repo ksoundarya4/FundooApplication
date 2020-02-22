ac3262538b45cc10962ae4f5bb2dcafc5f99b66b
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-22 15:49:46 +0530

    [soundarya]Refactor. Added RecyclerViewType into recycler_view_strategy module

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/recycler_view_strategy/RecyclerViewType.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/recycler_view_strategy/RecyclerViewType.kt
@@ -7,7 +7,7 @@
  * @version 1.0
  * @since 22/01/2020
  */
-package com.bridgelabz.fundoonotes.note_module.dashboard_page.model
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.view.recycler_view_strategy
 
 enum class RecyclerViewType {
     GridView, ListView
