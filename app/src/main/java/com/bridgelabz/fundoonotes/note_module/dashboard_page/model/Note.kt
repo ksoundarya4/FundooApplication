2a2d1d0fb511de0c72388d7c3eaf9d6fb658f143
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-23 20:08:23 +0530

    [soundarya]Refactor. added required fields to notes in Note.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/model/Note.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/model/Note.kt
@@ -11,4 +11,13 @@ package com.bridgelabz.fundoonotes.note_module.dashboard_page.model
 
 import java.io.Serializable
 
-data class Note(val title: String, val description: String) : Serializable
+data class Note(var title: String, var description: String) : Serializable {
+    var id: Int? = null
+    var isArchived: Int = 0
+    var isDeleted: Int = 0
+    var isPinned: Int = 0
+    var reminder: String? = null
+    var label: String? = null
+    var position: Int = 0
+    var colour: Int = 0
+}
