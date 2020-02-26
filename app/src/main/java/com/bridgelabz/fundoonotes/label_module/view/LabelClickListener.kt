8996b8cd40934b3f6486c4a15eeafd7fa0e298d2
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-26 18:31:28 +0530

    [soundarya]Add. Added LabelClickListener.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/label_module/view/LabelClickListener.kt
@@ -0,0 +1,14 @@
+/**
+ * Fundoo Notes
+ * @description ListViewClickListener Interface to perform
+ * onClick on label itemView.
+ * @file LabelClickListener.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 17/02/2020
+ */
+package com.bridgelabz.fundoonotes.label_module.view
+
+interface LabelClickListener {
+    fun onClick(adapterPosition : Int)
+}
