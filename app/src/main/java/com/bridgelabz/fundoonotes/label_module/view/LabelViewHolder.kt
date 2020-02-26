a9d7166bdde1c53820f609d32bd707d5f96d3bfd
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-26 15:40:00 +0530

    [soundarya]Add. Added LabelViewHolder.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/label_module/view/LabelViewHolder.kt
@@ -0,0 +1,24 @@
+/**
+ * Fundoo Notes
+ * @description LabelViewHolder class extends RecyclerView.ViewHolder
+ * @file LabelViewAdapter.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 17/02/2020
+ */
+package com.bridgelabz.fundoonotes.label_module.view
+
+import android.view.View
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import com.bridgelabz.fundoonotes.R
+import com.bridgelabz.fundoonotes.label_module.model.Label
+
+class LabelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val labelEditText: EditText = itemView.findViewById(R.id.text_label)
+
+    fun bindLabel(label: Label) {
+        labelEditText.setText(label.label)
+    }
+}
