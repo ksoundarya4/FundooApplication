809a3aa362fb3b862acd617c8efa367f242f22e0
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-26 19:22:06 +0530

    [soundarya]Refactor. Added label and checkLabel properties and click listeners for the same in LabelViewHolder.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/label_module/view/LabelViewHolder.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/label_module/view/LabelViewHolder.kt
@@ -10,6 +10,7 @@ package com.bridgelabz.fundoonotes.label_module.view
 
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import com.bridgelabz.fundoonotes.R
 import com.bridgelabz.fundoonotes.label_module.model.Label
@@ -17,6 +18,8 @@ import com.bridgelabz.fundoonotes.label_module.model.Label
 class LabelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val labelEditText: EditText = itemView.findViewById(R.id.text_label)
+    private val label: ImageButton = itemView.findViewById(R.id.label)
+    private val checkLabel: ImageButton = itemView.findViewById(R.id.check_label)
 
     fun bindLabel(label: Label) {
         labelEditText.setText(label.label)
@@ -24,7 +27,17 @@ class LabelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun onLabelClickListener(adapterPosition: Int, labelClickListener: LabelClickListener) {
         itemView.setOnClickListener {
+            label.setImageResource(R.drawable.ic_delete)
+            checkLabel.setImageResource(R.drawable.ic_check_grey)
             labelClickListener.onClick(adapterPosition)
+        }
+
+        label.setOnClickListener {
+            labelClickListener.onDeleteClick(adapterPosition)
+        }
+
+        checkLabel.setOnClickListener {
+            labelClickListener.onUpdateClick(adapterPosition)
         }
     }
 }
