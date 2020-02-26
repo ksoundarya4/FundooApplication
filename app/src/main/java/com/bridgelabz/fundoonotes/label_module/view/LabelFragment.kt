f99b2b4debc343f9d17549f51bc5dddfcd9ea530
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-26 16:03:55 +0530

    [soundarya]Refactor. Added observeLabels and initRecyclerView function to LabelFragment.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/label_module/view/LabelFragment.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/label_module/view/LabelFragment.kt
@@ -9,13 +9,32 @@ import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.bridgelabz.fundoonotes.R
+import com.bridgelabz.fundoonotes.label_module.model.Label
+import com.bridgelabz.fundoonotes.label_module.viewmodel.LabelViewModel
+import com.bridgelabz.fundoonotes.label_module.viewmodel.LabelViewModelFactory
+import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper
+import com.bridgelabz.fundoonotes.repository.local_service.lable_module.LabelTableManagerImpl
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class LabelFragment : Fragment() {
-
+    private val labelFactory by lazy {
+        LabelViewModelFactory(LabelTableManagerImpl(DatabaseHelper(requireContext())))
+    }
+    private val labelViewModel by lazy {
+        requireActivity().run {
+            ViewModelProvider(
+                this,
+                labelFactory
+            ).get(LabelViewModel::class.java)
+        }
+    }
     private val bottomAppBar by lazy {
         requireActivity().findViewById<BottomAppBar>(R.id.bottom_app_bar)
     }
@@ -28,9 +47,18 @@ class LabelFragment : Fragment() {
     private val clearButton by lazy {
         requireView().findViewById<ImageButton>(R.id.clear_label)
     }
+    private val saveButton by lazy {
+        requireView().findViewById<ImageButton>(R.id.save_label)
+    }
     private val labelEditText by lazy {
         requireView().findViewById<EditText>(R.id.edit_label)
     }
+    private val recyclerView by lazy {
+        requireView().findViewById<RecyclerView>(R.id.label_recyclerView)
+    }
+    private val labelAdapter = LabelViewAdapter(ArrayList())
+
+    private var labels = ArrayList<Label>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,11 +73,29 @@ class LabelFragment : Fragment() {
         hideBottomAppToolbar()
         setUpToolbar()
         setOnButtonClickListeners()
+        labelViewModel.getLabelLiveData().observe(requireActivity(), Observer { observeLabels(it) })
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.adapter = labelAdapter
+    }
+
+    private fun observeLabels(labelList: ArrayList<Label>) {
+        labels = labelList
+        labelAdapter.setLabelList(labels)
+        labelAdapter.notifyDataSetChanged()
     }
 
     private fun setOnButtonClickListeners() {
         clearButton.setOnClickListener {
             labelEditText.text.clear()
+        }
+        saveButton.setOnClickListener {
+            val label = labelEditText.editableText.toString()
+            labelViewModel.saveLabel(Label(label = label))
         }
     }
 
