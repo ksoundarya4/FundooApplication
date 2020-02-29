8c9f41e340a1aab9cc881696a86a2fe39b4fbc6d
ksoundarya4 <ksoundarya4@gmail.com>
2020-02-29 12:32:25 +0530

    [soundarya]Refactor. Added file comment into NotifyService.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/notification_util/NotifyService.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/notification_util/NotifyService.kt
@@ -1,6 +1,12 @@
 /**
  * Fundoo Notes
- *
+ *@description NotifyService pops a notification into
+ * the status bar. When user click the notification a
+ * dashboardActivity opens.
+ * @file NotifyService.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 29/02/2020
  */
 package com.bridgelabz.fundoonotes.notification_util
 
