4c80b351a4f592962cc21515dfdf04596a70cac3
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-03 16:04:02 +0530

    [soundarya]Refactor. Added createCardView function in NoteViewHolder.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteViewHolder.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/NoteViewHolder.kt
@@ -11,6 +11,8 @@ package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
 import android.view.View
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
 import com.bridgelabz.fundoonotes.R
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
@@ -20,8 +22,8 @@ class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val cardView: CardView = itemView.findViewById(R.id.note_card_view)
     private val title: TextView = itemView.findViewById(R.id.text_view_note_title)
     private val description: TextView = itemView.findViewById(R.id.text_view_note_description)
-    private val reminderCardView: CardView = itemView.findViewById(R.id.reminder_card_view)
-    private val reminderText: TextView = itemView.findViewById(R.id.reminder_text)
+    private val constraintLayout: ConstraintLayout =
+        itemView.findViewById(R.id.note_list_constraint_layout)
 
     fun bindNote(
         note: Note
@@ -29,10 +31,56 @@ class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         title.text = note.title
         description.text = note.description
 
-        if(note.reminder != null){
-            reminderText.text = note.reminder
-            reminderCardView.maxCardElevation = 4F
+        if (note.reminder != null) {
+            val reminder = note.reminder
+            createCardView(reminder!!)
         }
+    }
+
+    private fun createCardView(reminder: String) {
+        val reminderCardView = CardView(itemView.context)
+        reminderCardView.id = R.id.reminderCardView
+        reminderCardView.maxCardElevation = 4F
+
+        val reminderTextView = TextView(itemView.context)
+        reminderTextView.id = R.id.reminderTextView
+        reminderTextView.text = reminder
+
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(constraintLayout)
+
+        constraintSet.connect(
+            reminderCardView.id,
+            ConstraintSet.START,
+            constraintLayout.id,
+            ConstraintSet.START,
+            R.dimen.margin_16dp
+        )
+        constraintSet.connect(
+            reminderCardView.id,
+            ConstraintSet.END,
+            constraintLayout.id,
+            ConstraintSet.END,
+            R.dimen.margin_16dp
+        )
+        constraintSet.connect(
+            reminderCardView.id,
+            ConstraintSet.TOP,
+            description.id,
+            ConstraintSet.BOTTOM,
+            R.dimen.margin_8dp
+        )
+        constraintSet.connect(
+            reminderCardView.id,
+            ConstraintSet.BOTTOM,
+            constraintLayout.id,
+            ConstraintSet.BOTTOM,
+            R.dimen.margin_8dp
+        )
+
+        reminderCardView.addView(reminderTextView)
+        constraintLayout.addView(reminderCardView)
+        constraintSet.constrainWidth(constraintLayout.id,ConstraintSet.WRAP_CONTENT)
     }
 
     fun onNoteCLickListener(onNoteClick: OnNoteClickListener, adapterPosition: Int) {
