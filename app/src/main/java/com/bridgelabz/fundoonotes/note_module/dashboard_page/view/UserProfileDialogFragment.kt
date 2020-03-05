48a4697fca4b6f1509cf11511dde84028f0679c0
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-05 10:59:56 +0530

    [soundarya]Add. Added onResume function into UserProfileDialogFragment.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/UserProfileDialogFragment.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/UserProfileDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.bridgelabz.fundoonotes.R
 
@@ -14,5 +15,14 @@ class UserProfileDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_user_profile, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val params: ViewGroup.LayoutParams = dialog!!.window!!.attributes
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog!!.window!!.attributes = params as WindowManager.LayoutParams
     }
 }
