31a11c2046484a01aa0ae4835595b929efc9735f
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-05 10:57:28 +0530

    [soundarya]Add. Added UserProfileDialogFragment.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/UserProfileDialogFragment.kt
@@ -0,0 +1,18 @@
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.bridgelabz.fundoonotes.R
+
+class UserProfileDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+    }
+}
