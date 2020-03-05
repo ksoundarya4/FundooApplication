f89355fc88cdcda233d7a89e9f2ff2f0f4271c43
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-05 18:04:52 +0530

    [soundarya]Refactor. Removed Unused code from DatabaseHelper.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/local_service/DatabaseHelper.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/local_service/DatabaseHelper.kt
@@ -3,18 +3,10 @@ package com.bridgelabz.fundoonotes.repository.local_service
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.provider.BaseColumns
-import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper.UserNoteRegistrationContract.UserNote.TABLE_USER_NOTE
 import com.bridgelabz.fundoonotes.repository.local_service.lable_module.LabelTableManagerImpl.Companion.CREATE_TABLE_LABEL
 import com.bridgelabz.fundoonotes.repository.local_service.note_label_relation_module.NoteLabelTableManagerImpl.Companion.CREATE_TABLE_NOTE_LABEL
 import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteTableManagerImpl.NoteEntry.CREATE_NOTE_TABLE
 import com.bridgelabz.fundoonotes.repository.local_service.user_module.UserDbManagerImpl.UserEntry.CREATE_USER_TABLE
-
-private const val CREATE_USER_NOTE_TABLE =
-    "Create Table If Not Exists $TABLE_USER_NOTE (" +
-            " ${BaseColumns._ID} INTEGER PRIMARY KEY, " +
-            "${DatabaseHelper.UserNoteRegistrationContract.UserNote.KEY_USER_ID} INTEGER, " +
-            "${DatabaseHelper.UserNoteRegistrationContract.UserNote.KEY_NOTE_ID} INTEGER)"
 
 class DatabaseHelper(context: Context) :
     SQLiteOpenHelper(
@@ -60,13 +52,5 @@ class DatabaseHelper(context: Context) :
         const val VERSION_FOUR = 4
         const val DATABASE_VERSION = VERSION_THREE
         const val DATABASE_NAME = "App.db"
-    }
-
-    object UserNoteRegistrationContract {
-        object UserNote : BaseColumns {
-            const val TABLE_USER_NOTE = "User Notes"
-            const val KEY_USER_ID = "User_Id"
-            const val KEY_NOTE_ID = "Note_Id"
-        }
     }
 }
