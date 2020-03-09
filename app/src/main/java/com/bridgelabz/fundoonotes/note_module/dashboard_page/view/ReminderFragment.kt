cec322320e46972ec8133d76e98af84cbea9488f
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-09 11:50:59 +0530

    [soundarya]Refactor. Added ReminderFragment.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/ReminderFragment.kt
@@ -0,0 +1,80 @@
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
+
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bridgelabz.fundoonotes.R
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel.NoteTableManagerFactory
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel.SharedViewModel
+import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper
+import com.bridgelabz.fundoonotes.repository.local_service.note_module.NoteTableManagerImpl
+
+class ReminderFragment : Fragment(), OnNoteClickListener {
+
+    private val noteFactory: NoteTableManagerFactory by lazy {
+        NoteTableManagerFactory(NoteTableManagerImpl(DatabaseHelper(requireContext())))
+    }
+    private val sharedViewModel by lazy {
+        ViewModelProvider(this, noteFactory).get(SharedViewModel::class.java)
+    }
+
+    private val recyclerView: RecyclerView by lazy {
+        requireView().findViewById<RecyclerView>(R.id.notes_recycler_view)
+    }
+
+    private val noteAdapter = NoteViewAdapter(ArrayList(), this)
+
+    private val reminderNotes = ArrayList<Note>()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        sharedViewModel.getSimpleNoteLiveData()
+            .observe(requireActivity(), Observer { observeArchiveNotes(it) })
+        initRecyclerView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        (requireActivity() as AppCompatActivity).supportActionBar!!.title =
+            getString(R.string.app_bar_title_reminder_notes)
+    }
+
+    private fun initRecyclerView() {
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.adapter = noteAdapter
+    }
+
+    private fun observeArchiveNotes(noteList: ArrayList<Note>) {
+        val notes = noteList
+        for (note in notes) {
+            if (!note.reminder.isNullOrEmpty())
+                reminderNotes.add(note)
+        }
+        noteAdapter.setListOfNotes(reminderNotes)
+        noteAdapter.notifyDataSetChanged()
+    }
+
+    override fun onClick(adapterPosition: Int) {
+    }
+
+    override fun onLongClick(adapterPosition: Int) {
+
+    }
+}
