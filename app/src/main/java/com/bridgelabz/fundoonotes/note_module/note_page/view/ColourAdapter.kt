a11d1956ca2567dcfe45500cd9247f122290a26b
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-11 19:29:04 +0530

    [soundarya]Add. Added ColourAdapter.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/ColourAdapter.kt
@@ -0,0 +1,26 @@
+package com.bridgelabz.fundoonotes.note_module.note_page.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import com.bridgelabz.fundoonotes.R
+
+class ColourAdapter(private val colours: ArrayList<Int>) :
+    RecyclerView.Adapter<ColourViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColourViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.colour_list, parent, false)
+        return ColourViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return colours.size
+    }
+
+    override fun onBindViewHolder(holder: ColourViewHolder, position: Int) {
+        val colour = colours[position]
+        holder.bindColour(colour)
+    }
+}
