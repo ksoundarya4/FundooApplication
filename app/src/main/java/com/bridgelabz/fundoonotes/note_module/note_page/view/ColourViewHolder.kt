7aece9098a149a1180a670d22b76ae0bb1610b53
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-11 19:28:12 +0530

    [soundarya]Add. Added ColourViewHolder and ColourAdapter class

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/ColourViewHolder.kt
@@ -0,0 +1,17 @@
+package com.bridgelabz.fundoonotes.note_module.note_page.view
+
+import android.view.View
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.bridgelabz.fundoonotes.R
+
+class ColourViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val cardView = itemView.findViewById<CardView>(R.id.colour_card_view)
+    val imageButton = itemView.findViewById<ImageButton>(R.id.colour_button)
+
+    fun bindColour(colour: Int) {
+        cardView.setCardBackgroundColor(colour)
+        imageButton.setBackgroundColor(colour)
+    }
+}
