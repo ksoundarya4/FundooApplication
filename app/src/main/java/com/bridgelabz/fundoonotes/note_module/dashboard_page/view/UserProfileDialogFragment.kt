28f4f1e725d67ea104cc80d2b51234b36d106ce5
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-11 11:18:22 +0530

    [soundarya]Refactor. Removed GoogleSignIn Api from UserProfileDialogFragment.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/UserProfileDialogFragment.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/view/UserProfileDialogFragment.kt
@@ -1,8 +1,6 @@
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.view
 
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +10,8 @@ import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.bridgelabz.fundoonotes.R
 import com.bridgelabz.fundoonotes.user_module.registration.model.User
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.auth.api.signin.GoogleSignInResult
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.api.GoogleApiClient
 
-class UserProfileDialogFragment : DialogFragment(), GoogleApiClient.OnConnectionFailedListener {
+class UserProfileDialogFragment : DialogFragment() {
 
     private val userProfilePicture by lazy {
         requireView().findViewById<ImageView>(R.id.user_profile_pic)
@@ -30,7 +23,6 @@ class UserProfileDialogFragment : DialogFragment(), GoogleApiClient.OnConnection
         requireView().findViewById<TextView>(R.id.user_full_name_text)
     }
     private lateinit var user: User
-    private var googleApiClient: GoogleApiClient? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,39 +33,7 @@ class UserProfileDialogFragment : DialogFragment(), GoogleApiClient.OnConnection
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        getGoogleApiClient()
-    }
-
-    private fun getGoogleApiClient() {
-        val googleSignInOption =
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-        googleApiClient =
-            GoogleApiClient.Builder(requireActivity()).enableAutoManage(requireActivity(), this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOption).build()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val optionalPendingResult =
-            Auth.GoogleSignInApi.silentSignIn(googleApiClient)
-        if (optionalPendingResult.isDone) {
-            val result: GoogleSignInResult = optionalPendingResult.get()
-            handleGoogleSignInResult(result)
-        } else {
-            getUserArgument()
-        }
-    }
-
-    private fun handleGoogleSignInResult(result: GoogleSignInResult) {
-        if (result.isSuccess) {
-            val account = result.signInAccount
-            userFullName.text = account!!.displayName
-            userEmail.text = account.email
-            val imageUri: Uri? = account.photoUrl
-            userProfilePicture.setImageURI(imageUri)
-        } else {
-            getUserArgument()
-        }
+        getUserArgument()
     }
 
     private fun getUserArgument() {
@@ -96,15 +56,5 @@ class UserProfileDialogFragment : DialogFragment(), GoogleApiClient.OnConnection
         params.width = ViewGroup.LayoutParams.MATCH_PARENT
         params.height = ViewGroup.LayoutParams.WRAP_CONTENT
         dialog!!.window!!.attributes = params as WindowManager.LayoutParams
-    }
-
-    override fun onConnectionFailed(p0: ConnectionResult) {
-        Log.d("ReminderFragment", "Connection Failed")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        googleApiClient!!.stopAutoManage(requireActivity())
-        googleApiClient!!.disconnect()
     }
 }
