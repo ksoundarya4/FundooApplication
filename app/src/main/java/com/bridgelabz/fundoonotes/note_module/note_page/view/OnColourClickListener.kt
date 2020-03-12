6fda4730d2e3511160c182a1d97e0354582fb6bc
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-12 12:12:35 +0530

    [soundarya]Add. Added OnColourClickListener.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/OnColourClickListener.kt
@@ -0,0 +1,14 @@
+/**
+ * Fundoo Notes
+ * @description OnColourClickListener Interface to perform
+ * onClick on itemView of ColourDialogFragment
+ * @file OnColourClickListener.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 12/03/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.note_page.view
+
+interface OnColourClickListener {
+    fun onClick(position: Int)
+}
