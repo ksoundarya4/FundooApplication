3854da9da8c97bc106150d288278370bac3384c7
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-12 12:12:56 +0530

    [soundarya]Add. Added OnColourListener.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/OnColourListener.kt
@@ -0,0 +1,14 @@
+/**
+ * Fundoo Notes
+ * @description OnColourListener interface is used
+ * extract the colour from ColourDialogFragment.
+ * @file OnReminderListener.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 12/03/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.note_page.view
+
+interface OnColourListener {
+    fun onColourSubmit(colour: Int)
+}
