d11ed9662c3dd795c0442d6c6fdd204807706983
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-12 13:27:25 +0530

    [soundarya]Refactor. removed imageButton from ColourViewHolder.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/ColourViewHolder.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/note_page/view/ColourViewHolder.kt
@@ -8,11 +8,9 @@ import com.bridgelabz.fundoonotes.R
 
 class ColourViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val cardView = itemView.findViewById<CardView>(R.id.colour_card_view)
-    val imageButton = itemView.findViewById<ImageButton>(R.id.colour_button)
 
     fun bindColour(colour: Int) {
         cardView.setCardBackgroundColor(colour)
-        imageButton.setBackgroundColor(colour)
     }
 
     fun onColourButtonClick(onColourClickListener: OnColourClickListener, position: Int) {
