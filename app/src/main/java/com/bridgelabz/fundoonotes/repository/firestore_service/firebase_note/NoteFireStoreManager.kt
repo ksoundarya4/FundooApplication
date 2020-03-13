95b8b1cbddc2204de2bfefbb4df5bb658fb01235
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-13 13:10:11 +0530

    [soundarya]Refactor. Added file comment and insert,update,delete and fetch functions into NoteFireStoreManager.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/firestore_service/firebase_note/NoteFireStoreManager.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/firestore_service/firebase_note/NoteFireStoreManager.kt
@@ -1,4 +1,19 @@
+/**
+ * Fundoo Notes
+ * @description NoteFireStoreManager interface manages the
+ * CRUD operations related to cloud fire store.
+ * @file NoteFireStoreManager.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 13/03/2020
+ */
 package com.bridgelabz.fundoonotes.repository.firestore_service.firebase_note
 
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.model.Note
+
 interface NoteFireStoreManager {
+    fun insertNote(note: Note)
+    fun updateNote(note: Note)
+    fun deleteNote()
+    fun fetchNote(): Note
 }
