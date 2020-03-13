9144d3d377884d40176169074a983e7592ed11ee
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-13 19:14:39 +0530

    [soundarya]Refactor. Added fragment live data variable and setter and getter functions for the same in DashBoardViewModel.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/DashBoardViewModel.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/viewmodel/DashBoardViewModel.kt
@@ -9,9 +9,11 @@
  */
 package com.bridgelabz.fundoonotes.note_module.dashboard_page.viewmodel
 
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bridgelabz.fundoonotes.note_module.dashboard_page.view.NoteFragment
 import com.bridgelabz.fundoonotes.repository.local_service.DatabaseHelper
 import com.bridgelabz.fundoonotes.repository.local_service.user_module.UserDbManagerImpl
 import com.bridgelabz.fundoonotes.user_module.registration.model.User
@@ -20,6 +22,7 @@ class DashBoardViewModel(dbHelper: DatabaseHelper) : ViewModel() {
 
     private val userTableManager = UserDbManagerImpl(dbHelper)
     private val userLiverData = MutableLiveData<User>()
+    private val fragmentLiveData = MutableLiveData<Fragment>(NoteFragment())
 
     fun authenticatedUser(email: String) {
         userLiverData.value = userTableManager.fetchUser(email)
@@ -27,5 +30,13 @@ class DashBoardViewModel(dbHelper: DatabaseHelper) : ViewModel() {
 
     fun getUser(): LiveData<User> {
         return userLiverData
+    }
+
+    fun setFragmentLiveData(fragment: Fragment) {
+        fragmentLiveData.value = fragment
+    }
+
+    fun getFragmentLiveData(): LiveData<Fragment> {
+        return fragmentLiveData
     }
 }
