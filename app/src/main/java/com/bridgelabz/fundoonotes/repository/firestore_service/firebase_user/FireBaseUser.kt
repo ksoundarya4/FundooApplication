632948449919e9623c3bbf5a3d5d7a2a82388c24
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-13 19:17:14 +0530

    [soundarya]Refactor. Deleted FireBaseUser.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/firestore_service/firebase_user/FireBaseUser.kt
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.fundoonotes.repository.firestore_service.firebase_user
-
-data class FireBaseUser(private val name: String, private val email: String) {
-    private val id: String? = null
-}
