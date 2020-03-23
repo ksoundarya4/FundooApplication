d5a11587e6bc95bba045fdc38984f76c282fc97c
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-23 19:27:33 +0530

    [soundarya]Add. Added DataModelClass.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/api_service/DataModel.kt
@@ -0,0 +1,12 @@
+/**
+ * Fundoo Notes
+ * @description DataModel data class represents
+ * the data received from the server.
+ * @file DataModel
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 23/03/2020
+ */
+package com.bridgelabz.fundoonotes.repository.api_service
+
+data class DataModel(val data: ResponseModel)
