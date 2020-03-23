08a9989faaa44d0c9b6ce5770da832ed21d6da23
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-23 13:57:16 +0530

    [soundarya]Add. Changed get url address

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/server_service/JsonPlaceHolderApi.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/server_service/JsonPlaceHolderApi.kt
@@ -4,6 +4,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface JsonPlaceHolderApi {
-    @GET("getNotesList")
-    fun getNotesFromServer(): Call<List<NoteResponseModel>>
+    @GET("getNotesList?access_token=Ntr4sdRxow4lKOTdciFT63cue4ejHDWSpgx9JBKFNsJBdQ0BGALGnbHZucKHewPM")
+    fun getNotesFromServer(): Call<NoteResponseModel>
 }
