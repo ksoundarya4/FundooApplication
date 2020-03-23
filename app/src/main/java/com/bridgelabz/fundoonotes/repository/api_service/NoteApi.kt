431887f33fb0d3dfd1683ada486ba8cf88e26f6b
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-23 19:29:19 +0530

    [soundarya]Add. Added NoteApi.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/api_service/NoteApi.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/api_service/NoteApi.kt
@@ -1,9 +1,9 @@
-package com.bridgelabz.fundoonotes.repository.server_service
+package com.bridgelabz.fundoonotes.repository.api_service
 
 import retrofit2.Call
 import retrofit2.http.GET
 
-interface JsonPlaceHolderApi {
+interface NoteApi {
     @GET("getNotesList?access_token=Ntr4sdRxow4lKOTdciFT63cue4ejHDWSpgx9JBKFNsJBdQ0BGALGnbHZucKHewPM")
-    fun getNotesFromServer(): Call<NoteResponseModel>
+    fun getNotesFromServer(): Call<DataModel>
 }
