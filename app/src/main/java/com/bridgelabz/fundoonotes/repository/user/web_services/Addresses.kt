a3ea35ae9947d4556ffb9c6baec1f30c27cc85d1
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-29 13:22:22 +0530

    [soundarya]Add. Added Addresses.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/user/web_services/Addresses.kt
@@ -0,0 +1,11 @@
+/**
+ * Fundoo Notes
+ * @description Addresses data class contains address string.
+ * @file Addresses.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 29/03/2020
+ */
+package com.bridgelabz.fundoonotes.repository.user.web_services
+
+data class Addresses(var address: String? = null)
