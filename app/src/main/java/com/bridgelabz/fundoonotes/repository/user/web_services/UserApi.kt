e3c21f2fe9ff461e3312f0b6c720d3824b1fbc86
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-29 14:17:58 +0530

    [soundarya]Add. Added UserApi.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/user/web_services/UserApi.kt
@@ -0,0 +1,18 @@
+/**
+ * Fundoo Notes
+ * @description UserApi interface is used to communicate
+ * with web server.
+ * @file UserApi.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 29/03/2020
+ */
+package com.bridgelabz.fundoonotes.repository.user.web_services
+
+import retrofit2.Call
+import retrofit2.http.POST
+
+interface UserApi {
+    @POST("/user/login")
+    fun userLogin(): Call<UserSignUpModel>
+}
