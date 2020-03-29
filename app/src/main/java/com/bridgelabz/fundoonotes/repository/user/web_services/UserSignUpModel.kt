4a58dc62040cc3c45ad79aad9531ae76f9e14b97
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-29 14:15:54 +0530

    [soundarya]Add. Added UserSignUpModel.kt

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/user/web_services/UserSignUpModel.kt
@@ -0,0 +1,27 @@
+/**
+ * Fundoo Notes
+ * @description UserSignUpModel is data class that
+ * contains user information of logged in user.
+ * @file UserSignUpModel.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 29/03/2020
+ */
+package com.bridgelabz.fundoonotes.repository.user.web_services
+
+data class UserSignUpModel(
+    var firstName: String? = null,
+    var lastName: String? = null,
+    var phoneNumber: String? = null,
+    var imageUrl: String? = null,
+    var role: String? = null,
+    var service: String? = null,
+    var createdDate: String? = null,
+    var modifiedDate: String? = null,
+    var addresses: List<Addresses>? = null,
+    var realm: String? = null,
+    var username: String? = null,
+    var email: String? = null,
+    var emailVerified: Boolean = false,
+    var id: String? = null
+)
