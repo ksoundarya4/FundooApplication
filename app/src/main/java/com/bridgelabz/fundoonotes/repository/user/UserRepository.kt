30aceaa59bb6c711fe44aa75638ba573c4e7f7d4
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-30 15:36:56 +0530

    [soundarya]Refactor. Added return type to insertUser and fixed fetchUser function in UserRepository.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/user/UserRepository.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/user/UserRepository.kt
@@ -1,10 +1,14 @@
 package com.bridgelabz.fundoonotes.repository.user
 
+import androidx.lifecycle.LiveData
+import com.bridgelabz.fundoonotes.user_module.login.model.AuthState
+import com.bridgelabz.fundoonotes.user_module.registration.model.RegistrationStatus
 import com.bridgelabz.fundoonotes.user_module.registration.model.User
+import com.google.android.gms.auth.api.Auth
 
 interface UserRepository {
-    fun insertUser(user: User)
+    fun insertUser(user: User): LiveData<RegistrationStatus>
     fun updateUser(user: User)
     fun deleteUser(user: User)
-    fun fetchUser(email: String): User
+    fun fetchUser(email: String, password: String): LiveData<AuthState>
 }
