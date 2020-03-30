46460644b66952641884e69606f00e900d6822b7
ksoundarya4 <ksoundarya4@gmail.com>
2020-03-30 15:35:50 +0530

    [soundarya]Refactor. Codded fetchUser function and added getUser extension function to UserLoginRepositoryModel.kt

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/user/UserRepositoryImplementation.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/user/UserRepositoryImplementation.kt
@@ -1,11 +1,12 @@
 package com.bridgelabz.fundoonotes.repository.user
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.bridgelabz.fundoonotes.repository.local_service.user_module.UserDatabaseManager
-import com.bridgelabz.fundoonotes.repository.user.web_services.UserApi
-import com.bridgelabz.fundoonotes.repository.user.web_services.UserLoginModel
-import com.bridgelabz.fundoonotes.repository.user.web_services.UserSignUpModel
-import com.bridgelabz.fundoonotes.repository.user.web_services.UserSignUpResponseModel
+import com.bridgelabz.fundoonotes.repository.user.web_services.*
+import com.bridgelabz.fundoonotes.user_module.login.model.AuthState
+import com.bridgelabz.fundoonotes.user_module.registration.model.RegistrationStatus
 import com.bridgelabz.fundoonotes.user_module.registration.model.User
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,11 +18,15 @@ class UserRepositoryImplementation(
 ) : UserRepository {
     private val tag = "UserRepository"
 
-    override fun insertUser(user: User) {
+    override fun insertUser(user: User): LiveData<RegistrationStatus> {
+        val registrationStatus = MutableLiveData<RegistrationStatus>()
         val call = userApi.userSignUp(user.getUserSignUpModel())
+
         call.enqueue(object : Callback<UserSignUpResponseModel> {
+
             override fun onFailure(call: Call<UserSignUpResponseModel>, t: Throwable) {
                 Log.i(tag, t.message!!)
+                registrationStatus.value = RegistrationStatus.Failed
             }
 
             override fun onResponse(
@@ -30,14 +35,18 @@ class UserRepositoryImplementation(
             ) {
                 if (!response.isSuccessful) {
                     Log.i(tag, response.message())
+                    registrationStatus.value = RegistrationStatus.Failed
                     return
                 }
 
                 val userSignUpResponse = response.body() as UserSignUpResponseModel
                 Log.i(tag, userSignUpResponse.message!!)
+                userTableManager.insert(user)
+                registrationStatus.value = RegistrationStatus.Successful
             }
-
         })
+
+        return registrationStatus
     }
 
     override fun updateUser(user: User) {
@@ -48,9 +57,51 @@ class UserRepositoryImplementation(
 
     }
 
-    override fun fetchUser(email: String): User {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun fetchUser(email: String, password: String): LiveData<AuthState> {
+        val authState = MutableLiveData<AuthState>()
+        val user = userTableManager.fetchUser(email)
+        if (user != null) {
+            if (UserRepositoryUtils.isUserAuthenticated(
+                    user = user,
+                    email = email,
+                    password = password
+                )
+            ) {
+
+                val call = userApi.userLogin(user.getUserLoginModel())
+                call.enqueue(object : Callback<UserLoginResponseModel> {
+                    override fun onFailure(call: Call<UserLoginResponseModel>, t: Throwable) {
+                        Log.i(tag, t.message!!)
+                    }
+
+                    override fun onResponse(
+                        call: Call<UserLoginResponseModel>,
+                        response: Response<UserLoginResponseModel>
+                    ) {
+
+                        if (!response.isSuccessful) {
+                            Log.i(tag, response.code().toString())
+                            return
+                        }
+
+                        val userLoginResponseModel = response.body()
+                        val getUserWithServerId = userLoginResponseModel!!.getUser(user)
+                        userTableManager.update(user.id!!.toLong(), getUserWithServerId)
+                        authState.value = AuthState.AUTH
+                    }
+                })
+            } else {
+                authState.value = AuthState.AUTH_FAILED
+            }
+            authState.value = AuthState.NOT_AUTH
+        }
+        return authState
     }
+}
+
+private fun UserLoginResponseModel.getUser(user: User): User {
+    user.id = this.userId
+    return user
 }
 
 private fun User.getUserSignUpModel(): UserSignUpModel {
