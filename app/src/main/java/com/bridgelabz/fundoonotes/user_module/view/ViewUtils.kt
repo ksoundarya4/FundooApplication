b5e22535a5e1d3ac788fca8a5b14413234866759
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-01 20:35:20 +0530

    [soundarya]Refactor. Added isNetworkAvailable and showSnackbar functions to ViewUtils

--- app/src/main/java/com/bridgelabz/fundoonotes/user_module/view/ViewUtils.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/user_module/view/ViewUtils.kt
@@ -9,6 +9,9 @@
 package com.bridgelabz.fundoonotes.user_module.view
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +19,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ScrollView
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 /**Function to display toast message*/
 fun Context.toast(message: String) {
@@ -33,7 +37,7 @@ fun View.hideKeyboard() {
 fun setHideKeyboardOnTouch(context: Context, view: View) {
     try {
         if (!(view is EditText || view is ScrollView)) {
-            view.setOnTouchListener { view: View, motionEvent: MotionEvent ->
+            view.setOnTouchListener { view: View, _: MotionEvent ->
                 onTouch(view)
             }
         }
@@ -58,3 +62,27 @@ private fun onTouch(view: View): Boolean {
     return false
 }
 
+fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val network = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            //for other device how are able to connect with Ethernet
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            //for check internet over Bluetooth
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
+        }
+    } else {
+        val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+        return nwInfo.isConnected
+    }
+}
+
+fun setSnackBar(view: View, message: String) {
+    Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
+}
