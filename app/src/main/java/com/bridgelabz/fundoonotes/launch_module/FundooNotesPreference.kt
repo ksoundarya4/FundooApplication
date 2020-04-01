5581b8b87ded3a09fa49de3429657abd523b9de8
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-01 18:57:25 +0530

    [soundarya]Add. Added FundooNotesPreference object

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/launch_module/FundooNotesPreference.kt
@@ -0,0 +1,30 @@
+/**
+ * Fundoo Notes
+ * @description FundooNotesPreference object is used to set
+ * Shared Preference for Fundoo Notes app.
+ * @file FundooNotesPreference.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 01/04/2020
+ */
+package com.bridgelabz.fundoonotes.launch_module
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.bridgelabz.fundoonotes.R
+
+object FundooNotesPreference {
+
+    fun setPreference(context: Context): SharedPreferences {
+        return context.getSharedPreferences(
+            context.getString(R.string.app_name),
+            Context.MODE_PRIVATE
+        )
+    }
+
+    fun editPreference(preference: SharedPreferences, key: String, value: String) {
+        val editor = preference.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+}
