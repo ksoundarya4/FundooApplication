55ec3fdd936ec685c058f0d5f4323acb1064bf8a
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-02 18:10:36 +0530

    [soundarya]Add. Added NoteInsertionStatus

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/note_module/dashboard_page/model/NoteInsertionStatus.kt
@@ -0,0 +1,12 @@
+/**
+ * Fundoo Notes
+ * @description NoteInsertionStatus is a enum class that
+ * holds success or failure when note is uploaded to server.
+ * @file NoteInsertionStatus.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 02/04/2020
+ */
+package com.bridgelabz.fundoonotes.note_module.dashboard_page.model
+
+enum class NoteInsertionStatus { Success, Failure }
