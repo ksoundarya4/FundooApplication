5a6efbe695214d064c29c012f00cfbeee86aa24d
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-03 14:06:51 +0530

    [soundarya]Add. Added UpdateNoteResponseModel data class

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/note/web_service/UpdateNoteResponseModel.kt
@@ -0,0 +1,31 @@
+/**
+ * Fundoo Notes
+ * @Description UpdateNoteResponseModel is used to receive response
+ * from server after updating the note in server.
+ * @file UpdateNoteResponseModel.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 03/04/2020
+ */
+package com.bridgelabz.fundoonotes.repository.note.web_service
+
+data class UpdateNoteResponseModel(
+    var title: String? = null,
+    var description: String? = null,
+    var isPined: Boolean = false,
+    var isArchived: Boolean = false,
+    var isDeleted: Boolean = false,
+    var reminder: ArrayList<String>? = null,
+    var createdDate: String? = null,
+    var modifiedDate: String? = null,
+    var color: String? = null,
+    var label: ArrayList<String>? = null,
+    var typeOfNote: String? = null,
+    var checkList: ArrayList<String>? = null,
+    var imageUrl: String? = null,
+    var linkUrl: String? = null,
+    var collaborators: ArrayList<Any>? = null,
+    var id: String? = null,
+    var userId: String? = null,
+    var collaberator: ArrayList<Any>? = null
+)
