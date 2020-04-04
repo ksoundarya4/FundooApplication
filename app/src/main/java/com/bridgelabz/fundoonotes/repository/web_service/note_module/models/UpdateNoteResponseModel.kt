b476f48ef3dbe88d05b7d1fe059e5a29ca1c89fb
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-04 13:07:18 +0530

    [soundarya]Refactor.Fixed UpdateNoteResponseModel

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/web_service/note_module/models/UpdateNoteResponseModel.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/web_service/note_module/models/UpdateNoteResponseModel.kt
@@ -9,23 +9,4 @@
  */
 package com.bridgelabz.fundoonotes.repository.web_service.note_module.models
 
-data class UpdateNoteResponseModel(
-    var title: String? = null,
-    var description: String? = null,
-    var isPined: Boolean = false,
-    var isArchived: Boolean = false,
-    var isDeleted: Boolean = false,
-    var reminder: ArrayList<String>? = null,
-    var createdDate: String? = null,
-    var modifiedDate: String? = null,
-    var color: String? = null,
-    var label: ArrayList<String>? = null,
-    var typeOfNote: String? = null,
-    var checkList: ArrayList<String>? = null,
-    var imageUrl: String? = null,
-    var linkUrl: String? = null,
-    var collaborators: ArrayList<Any>? = null,
-    var id: String? = null,
-    var userId: String? = null,
-    var collaberator: ArrayList<Any>? = null
-)
+data class UpdateNoteResponseModel(var data: UpdateNoteResponseData)
