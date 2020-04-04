ec8b0ec01168b77a473e96d293b6bcdab827df01
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-04 13:08:31 +0530

    [soundarya]Add. added updateNoteData

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/web_service/note_module/models/UpdateNoteData.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/web_service/note_module/models/UpdateNoteData.kt
@@ -1,12 +1,15 @@
 /**
  * Fundoo Notes
- * @Description UpdateNoteResponseModel is used to receive response
+ * @Description UpdateNoteResponseData is used to receive response
  * from server after updating the note in server.
- * @file UpdateNoteResponseModel.kt
+ * @file UpdateNoteResponseData.kt
  * @author ksoundarya4
  * @version 1.0
  * @since 03/04/2020
  */
 package com.bridgelabz.fundoonotes.repository.web_service.note_module.models
 
-data class UpdateNoteResponseModel(var data: UpdateNoteData)
+data class UpdateNoteData(
+    var success: Boolean? = false,
+    var message: String? = null
+)
