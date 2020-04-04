a2078795ae1dd1a4388e429ed0b2e17bc6a21a3a
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-04 13:33:48 +0530

    [soundarya]Add. Added TrashNoteModel data class

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/web_service/note_module/models/TrashNoteModel.kt
@@ -0,0 +1,15 @@
+/**
+ * Fundoo Notes
+ * @description TrashNoteModel is used to add note to trash
+ * Note in Server.
+ * @file TrashNoteModel.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 03/04/2020
+ */
+package com.bridgelabz.fundoonotes.repository.web_service.note_module.models
+
+data class TrashNoteModel(
+    var isDeleted: Boolean = false,
+    var noteIdList: ArrayList<String>? = null
+)
