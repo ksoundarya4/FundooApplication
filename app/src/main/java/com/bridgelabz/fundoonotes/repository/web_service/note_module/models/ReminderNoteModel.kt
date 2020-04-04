5604e1b21684fcb959ed98bf266dfd543773d518
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-04 13:33:21 +0530

    [soundarya]Add. Added ReminderNoteModel data class

---
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/web_service/note_module/models/ReminderNoteModel.kt
@@ -0,0 +1,15 @@
+/**
+ * Fundoo Notes
+ * @description ReminderNoteModel is used to add reminder to
+ * Note in Server.
+ * @file ReminderNoteModel.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 03/04/2020
+ */
+package com.bridgelabz.fundoonotes.repository.web_service.note_module.models
+
+data class ReminderNoteModel(
+    var color: String? = null,
+    var noteIdList: ArrayList<String>? = null
+)
