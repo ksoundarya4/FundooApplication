3007610eb675b2529b6561e42732b93ccb265aa9
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-07 12:08:14 +0530

    [soundarya]Refactor.Added updatePassword function in UserRepositoryImplementation

--- app/src/main/java/com/bridgelabz/fundoonotes/repository/common/UserRepository.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/repository/common/UserRepository.kt
@@ -8,6 +8,7 @@ import com.bridgelabz.fundoonotes.user_module.model.User
 interface UserRepository {
     fun insertUser(user: User): LiveData<RegistrationStatus>
     fun updateUser(user: User)
+    fun updatePassword(password: String, accessToken: String): Boolean
     fun deleteUser(user: User)
     fun fetchUser(email: String, password: String): LiveData<AuthState>
     fun fetchUserFromLocalDb(email: String): User
