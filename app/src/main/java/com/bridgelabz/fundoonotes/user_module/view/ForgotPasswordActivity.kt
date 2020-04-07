08b261c480f50f63e7d97bef2e8d69b744973ace
ksoundarya4 <ksoundarya4@gmail.com>
2020-04-07 12:23:13 +0530

    [soundarya]Refactor.Added userViewModel to ForgotPasswordActivity

--- app/src/main/java/com/bridgelabz/fundoonotes/user_module/view/ForgotPasswordActivity.kt
+++ app/src/main/java/com/bridgelabz/fundoonotes/user_module/view/ForgotPasswordActivity.kt
@@ -2,7 +2,6 @@ package com.bridgelabz.fundoonotes.user_module.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +9,11 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bridgelabz.fundoonotes.R
+import com.bridgelabz.fundoonotes.launch_module.FundooNotesPreference
 import com.bridgelabz.fundoonotes.note_module.dashboard_page.view.HomeDashBoardActivity
-import com.bridgelabz.fundoonotes.user_module.login.viewmodel.AuthViewModel
 import com.bridgelabz.fundoonotes.user_module.model.validateConfirmPassword
+import com.bridgelabz.fundoonotes.user_module.viewModel.UserViewModel
+import com.bridgelabz.fundoonotes.user_module.viewModel.UserViewModelFactory
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
@@ -22,14 +23,22 @@ class ForgotPasswordActivity : AppCompatActivity() {
     private val confirmPasswordEditText by lazy {
         findViewById<EditText>(R.id.new_confirm_password_edit_text)
     }
-    private val submiteButton by lazy {
+    private val submitButton by lazy {
         findViewById<Button>(R.id.buttom_new_password_submit)
     }
-    private val viewModel by lazy {
-        ViewModelProvider(this).get(AuthViewModel::class.java)
+    private val userViewModelFactory by lazy {
+        UserViewModelFactory(
+            this
+        )
+    }
+    private val userViewModel by lazy {
+        ViewModelProvider(this, userViewModelFactory).get(UserViewModel::class.java)
     }
     private val forgotPasswordActivity by lazy {
         findViewById<ConstraintLayout>(R.id.forgot_password_constraint_layout)
+    }
+    private val preference by lazy {
+        FundooNotesPreference.getPreference(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +54,8 @@ class ForgotPasswordActivity : AppCompatActivity() {
 
     private fun setSubmitButtonClickListener() {
         val email = intent.getStringExtra("email")
-        submiteButton.setOnClickListener {
+        val accessToken = preference.getString("access_token", null)
+        submitButton.setOnClickListener {
             val newPassword = newPasswordEditText.editableText.toString()
             val newConfirmPassword = confirmPasswordEditText.editableText.toString()
             if (validateConfirmPassword(
@@ -53,8 +63,9 @@ class ForgotPasswordActivity : AppCompatActivity() {
                     newConfirmPassword
                 )
             ) {
-                viewModel.onPasswordSubmitButtonClick(View(this), email!!, newPassword)
-                viewModel.getUpdateStatus().observe(this, Observer { handleUpdatePassword(it) })
+                userViewModel.updateNewPassword(email!!, newPassword, accessToken!!)
+                userViewModel.getUpdatePasswordStatus()
+                    .observe(this, Observer { handleUpdatePassword(it) })
             } else {
                 confirmPasswordEditText.error = "Did not match password"
             }
